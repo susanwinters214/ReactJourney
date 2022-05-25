c291093b0435383c2d8e4b7449e2c166a990e8f3
Susan Winters <susanwinters214@gmail.com>
2022-05-25 14:09:40 -0500

    add h3 to home page

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Things you need to do before starting this project:
 1. Download and install Visual Studio Code (https://code.visualstudio.com/)
 2. Download and install NodeJS (https://nodejs.org/en/)
 3. Go setup a Profile on GitHub (https://github.com/)
+
+May The Force Be With You!
--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h3>React Journey</h3>
+        <h4>2022</h4>
         <p>
           Hello World! My name is Susan Winters. I love ReactJS!!!!
         </p>
