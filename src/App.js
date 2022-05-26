c7033bc9e87f4034ad6d650e4e91947da3352d9d
Susan Winters <susanwinters214@gmail.com>
2022-05-25 19:20:30 -0500

    added JSX to the page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = "React Journey";
+  
+  const person = {
+    fullname: "Susan Winters",
+    lang: "ReactJS"
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h3>React Journey</h3>
-        <h4>2022</h4>
+        <h1>{title}</h1>
+        <h2>2022</h2>
         <p>
-          Hello World! My name is Susan Winters. I love ReactJS!!!!
+          Hello World! My name is {person.fullname}. I love {person.lang} !!!!
         </p>
         <a
           className="App-link"
